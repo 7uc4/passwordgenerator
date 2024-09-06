8c2c64e9572608c42facddb63c361c4db714ed88
7uc4 <63272605+7uc4@users.noreply.github.com>
2024-09-06 19:23:52 -0300

    Update simple-password-generator.sh

--- simple-password-generator.sh
+++ simple-password-generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Simple password generator
-# This script generates a new password based on date comand (nanoseconds and seconds since the Epoch formats) and sha256 hash.
+# This script generates a new password based on date command (nanoseconds and seconds since the Epoch formats) and sha256 hash.
 # You will be prompted to enter the password lenght the user need.
 # The generated password will be displayed at the end.
 
